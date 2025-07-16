5b9dc0abcc4017272f970a7e1f50a657978604ef
yattorie <pyryryqw882@gmail.com>
2025-07-16 15:24:33 +0300

    Initial commit

---
+++ src/main/java/com/orlovandrei/App.java
@@ -0,0 +1,13 @@
+package com.orlovandrei;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
