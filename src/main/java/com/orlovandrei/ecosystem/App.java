7fcc085da03afbf3c518beb0bf35454f8ba91a36
yattorie <pyryryqw882@gmail.com>
2025-08-11 21:35:28 +0300

    refactor: move App to ecosystem directory

---
+++ src/main/java/com/orlovandrei/ecosystem/App.java
@@ -0,0 +1,11 @@
+package com.orlovandrei.ecosystem;
+
+import com.orlovandrei.ecosystem.api.Ecosystem;
+
+public class App
+{
+    public static void main(String[] args) {
+        Ecosystem ecosystem = new Ecosystem();
+        ecosystem.startEcoSystem();
+    }
+}
